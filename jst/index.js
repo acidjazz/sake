e6b0e999f6f34eec879af5fa49cb2705e1c944fc
kevin olson <acidjazz@gmail.com>
2016-02-01 17:45:30 -0800

    latest

---
+++ jst/index.js
@@ -0,0 +1,9 @@
+var Index;
+
+Index = {
+  i: function() {
+    return console.log('Index.i()');
+  }
+};
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmNvZmZlZSJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxJQUFBOztBQUFBLEtBQUEsR0FFRTtFQUFBLENBQUEsRUFBRyxTQUFBO1dBRUQsT0FBTyxDQUFDLEdBQVIsQ0FBWSxXQUFaO0VBRkMsQ0FBSCIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZVJvb3QiOiIvc291cmNlLyIsInNvdXJjZXNDb250ZW50IjpbIkluZGV4ID1cblxuICBpOiAtPlxuXG4gICAgY29uc29sZS5sb2cgJ0luZGV4LmkoKSdcbiJdfQ==
